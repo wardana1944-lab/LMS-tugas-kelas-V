8e40d2e72cfa6f088d8e834a5fc3c2b0c1af15af
wardana1944-lab <wardana1944@gmail.com>
2026-03-29 15:40:12 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,31 @@
-const CACHE_NAME = 'ruang-belajar-cache-v1';
+const CACHE_NAME = 'ruang-belajar-v3';
 const ASSETS = [
-  '/',
-  './FIX%20TERBARU.html',
+  './',
+  './index.html',
   './logo.png',
   './manifest.json'
 ];
 
-// Install
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+        keys.filter((key) => key !== CACHE_NAME).map((k) => caches.delete(k))
       );
     })
   );
   return self.clients.claim();
 });
 
-// Fetch (Network First Strategy)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
