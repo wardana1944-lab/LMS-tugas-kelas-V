8c31854bff571f480f8e06fb73b1df1af16c709f
wardana1944-lab <wardana1944@gmail.com>
2026-09-20 15:03:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+window.APP_CONFIG = {
+  APP_VERSION: "v2.6.0",
+  firebaseConfig: {
+    apiKey: "AIzaSyCfzJJpq32AltXRcP0ZB6fM1mvYpJVSVl4",
+    authDomain: "ruang-belajar-kelas-v.firebaseapp.com",
+    databaseURL: "https://ruang-belajar-kelas-v-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ruang-belajar-kelas-v",
+    storageBucket: "ruang-belajar-kelas-v.firebasestorage.app",
+    messagingSenderId: "397105486119",
+    appId: "1:397105486119:web:4cfc9ad92dd36fed42b102",
+    measurementId: "G-69F6KWF0ZH"
+  },
+  defaultTeacher: {
+    email: "wardana19442@gmail.com",
+    pin: "guru123",
+    nama: "Aditya W. Wardana, S.Pd",
+    nip: "NIP 19940319 202321 1 005",
+    sekolah: "SEKOLAH DASAR NEGERI PENGKOL",
+    tahun: "2025/2026"
+  }
+};
